78e21eea194b1037a406134ac7036a46fba276e9
Piotr Wieżel <piotrw76@gmail.com>
2020-04-03 22:03:17 +0200

    Add files via upload

---
+++ fileReader.py
@@ -0,0 +1,21 @@
+import os
+
+class fileReader:
+    def readLines(inputFile):
+        __location__ = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+        f = open(os.path.join(__location__, inputFile))
+
+        lines = f.readlines()
+
+        for i, line in enumerate(lines):
+            lines[i] = lines[i].rstrip("\n")
+
+        return lines
+
+if __name__ == '__main__':
+    l = fileReader
+    labels = l.readLines("labels.txt")
+    for label in labels:
+        print(label)
---
+++ hpccoutExctracter.py
@@ -0,0 +1,24 @@
+import fileReader
+import os
+
+class hpccoutExctacter:
+    __inputLabelsFilename = "labels.txt"
+    __hpccoutFilename = "hpccout.txt"
+
+    def extractLabelsWithValues(self):
+        def createEmptyHashMap(self):
+            l = fileReader
+            labels = fileReader.fileReader.readLines(self.__hpccoutFilename)
+
+            #TODO: Create an empty hash map
+            #Fill it with values from read file
+
+        def extractHpccOutput(self, labelsAndValues):
+    #TODO
+
+
+if __name__ == '__main__':
+    l = hpccoutExctacter
+    lines = l.extractLabelsWithValues("labels.txt")
+    for line in lines:
+        print(line)
